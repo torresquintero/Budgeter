64e34602e79edd2453de5f94de403ed5965d38b7
torresquintero <alexandra.torresquintero@gmail.com>
2016-12-12 23:56:06 -0500

    initializing version control

--- Budgeter/ViewController.swift
+++ Budgeter/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var clickCount = 0
+    var timer = Timer()
+    
 
+    @IBAction func securityTapButton(_ sender: Any) {
+        clickCount += 1
+        
+        print(clickCount)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // called every time interval from the timer
+    func timerAction() {
+        timer.invalidate()
+        if clickCount == 4 {
+            self.performSegue(withIdentifier: "firstSegue", sender: nil)
+        }
+        else {
+            print("ran out of time")
+            fatalError()
+        }
     }
 
 
